6f6cc440583750b6b50fe60e57b49580f8a2a72d
Huge Rockr <hieuhpm@vng.com.vn>
2018-02-02 10:47:04 +0700

    Add nexus credentials configuration

--- buildscripts/generate_gradle_properties.sh
+++ buildscripts/generate_gradle_properties.sh
@@ -1,4 +1,6 @@
 echo "
 gradle.publish.key=${GRADLE_PUBLISH_KEY}
 gradle.publish.secret=${GRADLE_PUBLISH_SECRET}
+NEXUS_USERNAME=${NEXUS_USERNAME}
+NEXUS_PASSWORD=${NEXUS_PASSWORD}
 " > gradle.properties
